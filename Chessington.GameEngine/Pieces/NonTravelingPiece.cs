1cc25a242d1420dd097b5f8d568317dc411ffea3
EthanLMartin <Ethan.Martin@Softwire.com>
2019-09-09 15:36:42 +0100

    Added TravelingPiece and NonTraveling Piece abstract classes

---
+++ Chessington.GameEngine/Pieces/NonTravelingPiece.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chessington.GameEngine.Pieces
+{
+    public abstract class NonTravelingPiece : Piece
+    {
+        protected NonTravelingPiece(Player player)
+            : base(player) { }
+
+        protected List<Square> RemoveFriendlyTakes(List<Square> moves, Board board)
+        {
+            moves.RemoveAll(s => (board.GetPiece(s) != null) && (board.GetPiece(s).Player == this.Player));
+            return moves;
+        }
+    }
+}
---
+++ Chessington.GameEngine/Pieces/TravelingPiece.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chessington.GameEngine.Pieces
+{
+    public abstract class TravelingPiece : Piece
+    {
+        protected TravelingPiece(Player player) 
+            : base(player) { }
+
+        protected List<Square> GetLineOffsetMovements(Square location, Board board, Direction direction)
+        {
+            List<Square> moves = new List<Square>();
+
+            Square newLocation = location + direction;
+
+            while (board.IsWithinBounds(newLocation))
+            {
+                if (!board.IsEmpty(newLocation))
+                {
+                    if (board.GetPiece(newLocation).Player != this.Player)
+                    {
+                        moves.Add(newLocation);
+                    }
+                    break;
+                }
+
+                moves.Add(newLocation);
+
+                newLocation += direction;
+            }
+
+            return moves;
+        }
+    }
+}
