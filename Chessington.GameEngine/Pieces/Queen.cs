2fc7737ecd4bf93d3f823c82e025e0583164d95f
EthanLMartin <Ethan.Martin@Softwire.com>
2019-09-09 10:54:25 +0100

    Added movement of the Queen piece

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -10,7 +10,55 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> moves = new List<Square>();
+            Square location = board.FindPiece(this);
+
+            moves.AddRange(GetDiagonalMoves(location));
+            moves.AddRange(GetLateralMoves(location));
+
+            moves = ClipToBoard(moves, board);
+
+            return moves;
+        }
+
+        private List<Square> GetDiagonalMoves(Square location)
+        {
+            List<Square> moves = new List<Square>();
+
+            for (int i = 1; i < GameSettings.BoardSize; i++)
+            {
+                moves.Add(new Square(location.Row + i, location.Col + i));
+                moves.Add(new Square(location.Row + i, location.Col - i));
+                moves.Add(new Square(location.Row - i, location.Col + i));
+                moves.Add(new Square(location.Row - i, location.Col - i));
+            }
+
+            return moves;
+        }
+
+        private List<Square> GetLateralMoves(Square location)
+        {
+            List<Square> moves = new List<Square>();
+
+            for (int row = 0; row < GameSettings.BoardSize; row++)
+            {
+                moves.Add(new Square(row, location.Col));
+            }
+
+            for (int col = 0; col < GameSettings.BoardSize; col++)
+            {
+                moves.Add(new Square(location.Row, col));
+            }
+
+            moves.RemoveAll(s => s == location);
+
+            return moves;
+        }
+
+        private List<Square> ClipToBoard(List<Square> moves, Board board)
+        {
+            moves.RemoveAll(s => !board.IsWithinBounds(s));
+            return moves;
         }
     }
 }
