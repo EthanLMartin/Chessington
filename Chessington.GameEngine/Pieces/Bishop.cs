fc852c2deeb376a9be493708ff9f1387e0e89800
EthanLMartin <Ethan.Martin@Softwire.com>
2019-09-09 12:18:47 +0100

    Code refactoring

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -11,22 +11,81 @@ namespace Chessington.GameEngine.Pieces
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
             Square location = board.FindPiece(this);
-            List<Square> moves = GetDiagonalMoves(location);
+            List<Square> moves = new List<Square>();
+
+            moves.AddRange(GetDiagonalNEMoves(location, board));
+            moves.AddRange(GetDiagonalSEMoves(location, board));
+            moves.AddRange(GetDiagonalNWMoves(location, board));
+            moves.AddRange(GetDiagonalSWMoves(location, board));
+
             moves = ClipToBoard(moves, board);
 
             return moves;
         }
 
-        private List<Square> GetDiagonalMoves(Square location)
+        private List<Square> GetDiagonalSEMoves(Square location, Board board)
         {
             List<Square> moves = new List<Square>();
 
             for (int i = 1; i < GameSettings.BoardSize; i++)
             {
-                moves.Add(new Square(location.Row + i, location.Col + i));
-                moves.Add(new Square(location.Row + i, location.Col - i));
-                moves.Add(new Square(location.Row - i, location.Col + i));
-                moves.Add(new Square(location.Row - i, location.Col - i));
+                Square loc = new Square(location.Row - i, location.Col + i);
+                moves.Add(loc);
+                if (!board.IsWithinBounds(loc) || !board.IsEmpty(location.Row - i, location.Col + i))
+                {
+                    break;
+                }
+            }
+
+            return moves;
+        }
+
+        private List<Square> GetDiagonalNEMoves(Square location, Board board)
+        {
+            List<Square> moves = new List<Square>();
+
+            for (int i = 1; i < GameSettings.BoardSize; i++)
+            {
+                Square loc = new Square(location.Row + i, location.Col + i);
+                moves.Add(loc);
+                if (!board.IsWithinBounds(loc) || !board.IsEmpty(location.Row + i, location.Col + i))
+                {
+                    break;
+                }
+            }
+
+            return moves;
+        }
+
+        private List<Square> GetDiagonalSWMoves(Square location, Board board)
+        {
+            List<Square> moves = new List<Square>();
+
+            for (int i = 1; i < GameSettings.BoardSize; i++)
+            {
+                Square loc = new Square(location.Row + i, location.Col - i);
+                moves.Add(loc);
+                if (!board.IsWithinBounds(loc) || !board.IsEmpty(location.Row + i, location.Col - i))
+                {
+                    break;
+                }
+            }
+
+            return moves;
+        }
+
+        private List<Square> GetDiagonalNWMoves(Square location, Board board)
+        {
+            List<Square> moves = new List<Square>();
+
+            for (int i = 1; i < GameSettings.BoardSize; i++)
+            {
+                Square loc = new Square(location.Row - i, location.Col - i);
+                moves.Add(loc);
+                if (!board.IsWithinBounds(loc) || !board.IsEmpty(location.Row - i, location.Col - i))
+                {
+                    break;
+                }
             }
 
             return moves;
